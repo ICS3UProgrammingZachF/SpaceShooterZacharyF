1a225028b9b06cf369c525efbbe2d42959728081
Zachfranks <43444948+Zachfranks@users.noreply.github.com>
2018-11-21 11:52:02 -0500

    add work

---
+++ Assets/scripts/PlayerContole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContole : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //rigidbody.velocity = some vector3 value;
+    }
+
+}
