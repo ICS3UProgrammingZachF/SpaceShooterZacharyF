db8910ca5eba1beb44f3f06fe039e08d022bffc1
Zachfranks <43444948+Zachfranks@users.noreply.github.com>
2018-11-21 17:12:00 -0500

    add this

--- Assets/scripts/PlayerContole.cs
+++ Assets/scripts/PlayerContole.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
+    public class Boundary
+    {
+     public float xMin, xMax, zMin, zMax;
+    }
 public class PlayerContole : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+    }
+
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        
 
-        //rigidbody.velocity = some vector3 value;
+        float moveVert = Input.GetAxis("Vertical");
+        float movehorizontal = Input.GetAxis("Horizontal");
+
+       Vector3 movement = new Vector3 (movehorizontal, 0.0f, moveVert);
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+            (
+                Mathf.Clamp (rb.position.x,boundary.xMin,boundary.xMax),
+                0.0f,
+                Mathf.Clamp (rb.position.z,boundary.zMin,boundary.zMax)
+             );
+        rb.rotation = Quaternion.Euler(0.0f,0.0f , rb.velocity.x * -tilt);
     }
+    
+
 
 }
